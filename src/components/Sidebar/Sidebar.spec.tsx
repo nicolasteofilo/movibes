4cd8acce13a49e5bc6526e1317fba31713c226a7
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-12-22 16:12:36 -0300

    test: ensure <Sidebar /> render links on/off

--- src/components/Sidebar/Sidebar.spec.tsx
+++ src/components/Sidebar/Sidebar.spec.tsx
@@ -1,16 +1,18 @@
 import { screen, render } from "@testing-library/react";
 import { renderWithTheme } from "../../utils/tests/helpers";
 
+import theme from "../../styles/theme";
+
 import { Sidebar } from ".";
 
 describe('<Sidebar />', () => {
-  it('should be render a logo', () => {
+  it('should render a logo', () => {
     renderWithTheme(<Sidebar currentLocation="/" />);
 
     expect(screen.getByAltText(/Movibes/i));
   })
 
-  it('should be render list of links', () => {
+  it('should render list of links', () => {
     renderWithTheme(<Sidebar currentLocation="/" />);
 
     const homeLink = screen.getByRole('link', { name: /Início/ });
@@ -18,5 +20,19 @@ describe('<Sidebar />', () => {
 
     expect(homeLink);
     expect(filmsLink);
+  })
+
+  it('should render link on/off', () => {
+    renderWithTheme(<Sidebar currentLocation="/" />);
+
+    const homeLink = screen.getByRole('link', {name: /Início/});
+    const filmsLink = screen.getByRole('link', {name: /Filmes/});
+
+    expect(homeLink).toHaveStyle({
+      color: theme.colors.green,
+    })
+    expect(filmsLink).toHaveStyle({
+      color: theme.colors.gray,
+    })
   })
 })
