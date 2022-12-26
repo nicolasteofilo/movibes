440c5f46d8377ae10d032b8e0031cf570ba241c5
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-12-26 18:43:23 -0300

    test: ensure <Header /> render the avatar

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -3,10 +3,20 @@ import { screen } from "@testing-library/react";
 import { renderWithTheme } from "../../utils/tests/helpers";
 import { Header } from ".";
 
+const user = {
+  name: "Nicolas", imageUrl: "https://avatars.githubusercontent.com/u/81480818?v=4"
+}
+
 describe('<Header />', () => {
   it('should be able render search input', () => {
-    renderWithTheme(<Header />);
+    renderWithTheme(<Header user={user} />);
 
     expect(screen.getByPlaceholderText(/Pesquise por filmes, séries.../i))
+  })
+
+  it('should be able render the avatar image', () => {
+    renderWithTheme(<Header user={user} />);
+
+    expect(screen.getByRole('img', { name: /Nicolas/i }))
   })
 })
