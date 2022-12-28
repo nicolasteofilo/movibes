e92e01a3d58f3b7daf0c8933e1aa134d6f58fe6e
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-12-28 14:17:44 -0300

    feat: ensure correct rendering of <MovieCard />

---
+++ src/components/MovieCard/MovieCard.spec.tsx
@@ -0,0 +1,24 @@
+import { screen } from "@testing-library/react";
+
+import { renderWithTheme } from "../../utils/tests/helpers";
+import { MovieCard } from ".";
+
+const film = {
+  name: "Top Gun: Maverick",
+  rate: 8.4,
+  coverUrl:
+    "https://upload.wikimedia.org/wikipedia/pt/thumb/d/d2/Top_Gun_Maverick.jpg/250px-Top_Gun_Maverick.jpg",
+};
+
+describe("<MovieCard />", () => {
+  it('should be render a card for the movie', () => {
+    renderWithTheme(<MovieCard {...film} />);
+
+    expect(screen.getByLabelText(/Top Gun: Maverick/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/estrela/i)).toBeInTheDocument();
+    expect(screen.getByText(/8.4/i)).toBeInTheDocument();
+    expect(screen.getByTitle(/Top Gun: Maverick/i)).toHaveStyle({
+      'background-image': `url("${film.coverUrl}")`,
+    })
+  })
+});
