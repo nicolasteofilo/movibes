11b63a3447cc5687c23ea09efdf647c673e20360
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-12-28 14:30:16 -0300

    test: ensure changes its value when typing in <TextField />

--- src/components/TextField/TextField.spec.tsx
+++ src/components/TextField/TextField.spec.tsx
@@ -1,26 +1,41 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { SearchImg } from "../../assets/icons/search";
 
 import { renderWithTheme } from "../../utils/tests/helpers";
 import { TextField } from ".";
 
-describe('<TextField />', () => {
-    it('should be able render with placeholder', () => {
-      renderWithTheme(<TextField placeholder="hey you" />);
+describe("<TextField />", () => {
+  it("should be able render with placeholder", () => {
+    renderWithTheme(<TextField placeholder="hey you" />);
 
-      expect(screen.getByPlaceholderText(/hey you/i)).toBeInTheDocument();
-    })
+    expect(screen.getByPlaceholderText(/hey you/i)).toBeInTheDocument();
+  });
 
-    it('should be able render with icon', () => {
-      renderWithTheme(<TextField icon={<SearchImg data-testid="icon" />} />);
+  it("should be able render with icon", () => {
+    renderWithTheme(<TextField icon={<SearchImg data-testid="icon" />} />);
 
-      expect(screen.getByTestId('icon')).toBeInTheDocument()
-    })
+    expect(screen.getByTestId("icon")).toBeInTheDocument();
+  });
 
-    it('should be able render without icon', () => {
-      renderWithTheme(<TextField />)
+  it("should be able render without icon", () => {
+    renderWithTheme(<TextField />);
 
-      expect(screen.queryByTestId('icon')).not.toBeInTheDocument()
-  })
-})
+    expect(screen.queryByTestId("icon")).not.toBeInTheDocument();
+  });
+
+  it("changes its value when typing", async () => {
+    const onChangeMock = jest.fn();
+    renderWithTheme(<TextField onChange={onChangeMock} />);
+
+    const input = screen.getByRole("textbox");
+    const text = "Hello Word";
+    userEvent.type(input, text);
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text);
+      expect(onChangeMock).toHaveBeenCalledTimes(text.length);
+    });
+  });
+});
