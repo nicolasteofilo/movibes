00e8f8ddf56d68c2619e8996d12db48ba49238c4
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-01-08 16:10:54 -0300

    refactor: remove contextAPI usage

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { SearchProvider } from './contexts/SearchContext'
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <SearchProvider>
-      <App />
-    </SearchProvider>
+    <App />
   </React.StrictMode>,
 )
